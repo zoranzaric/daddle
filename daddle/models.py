c0047cd7a314741d11f6506ba4b2f4e5e254de44
Zoran Zaric <zz@zoranzaric.de>
2015-10-12 15:56:28 +0200

    Add event model

--- daddle/models.py
+++ daddle/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    start_date = models.DateTimeField()
+    min_people = models.IntegerField()
+    max_people = models.IntegerField()
+
+    def __str__(self):
+        return "%s (%s)" % (self.title, self.start_date)
+

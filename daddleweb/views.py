2a8a21328cdadbbf674ebd0087e5955458bcbea4
Zoran Zaric <zz@zoranzaric.de>
2015-10-12 18:05:39 +0200

    Add basic index page

--- daddleweb/views.py
+++ daddleweb/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from daddle.models import Event
 
-# Create your views here.
+import datetime
+
+def index(request):
+    events = Event.objects.filter(start_date__gte=datetime.datetime.now())
+    context = {'events': events}
+    return render(request, 'index.html', context)

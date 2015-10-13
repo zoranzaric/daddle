6f972bf4e7891a2d7100e759cdcac6d5d6ea3c8c
Zoran Zaric <zz@zoranzaric.de>
2015-10-13 10:13:22 +0200

    Add setup instructions

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ daddle is a tool to organize raids or other events with multiple people.
 
 I wrote daddle to easier organize raids with my Destiny clan.
 
+Setup
+-----
+
+daddle is a basic Django app.
+
+The ENV variable `DADDLE_SECRET_KEY` has to be set.
+
--- daddleproject/settings.py
+++ daddleproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4n3laqz(0t^8t$eulzc=$i0-!smu0tj9%chdm6135h=p#espz2'
+SECRET_KEY = os.environ.get('DADDLE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

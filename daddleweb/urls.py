b6499ca8c7296e366a90401bb5d86bcf043d6adb
Zoran Zaric <zz@zoranzaric.de>
2015-10-13 17:35:04 +0200

    Add login feature

--- daddleweb/urls.py
+++ daddleweb/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
@@ -7,5 +9,14 @@ urlpatterns = [
     url(r'^welcome', views.welcome, name='welcome'),
     url('^pledge', views.pledge_user_to_event, name='pledge'),
     url('^cancel_pledge', views.cancel_pledge, name='cancel_pledge'),
+
+    url(r'^login?/',
+        auth_views.login,
+        {'template_name': 'login.html'},
+        name='auth_login'),
+    url(r'^logout?/',
+        auth_views.logout,
+        {'template_name': 'logout.html'},
+        name='auth_logout'),
 ]
 
